fbec55621c02667f735fde4e655a353ddb535d6d
Sindhu Elango <sindhuocs22@gmail.com>
2020-03-09 11:14:36 +0530

    Localization file added

---
+++ ios Assignment/Common/Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Extensions.swift
+//  ios Assignment
+//
+//  Created by INFOSYS on 09/03/20.
+//  Copyright © 2020 INFOSYS. All rights reserved.
+//
+import UIKit
+
+extension UITableView {
+  
+  func setEmptyMessage(_ message: String) {
+    let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+    messageLabel.text = message
+    messageLabel.textColor = .black
+    messageLabel.numberOfLines = 0
+    messageLabel.textAlignment = .center
+    messageLabel.font = UIFont.systemFont(ofSize: 11)
+    messageLabel.sizeToFit()
+    
+    self.backgroundView = messageLabel
+  }
+  
+  func restore() {
+    self.backgroundView = nil
+  }
+}
