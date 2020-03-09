4e6ae7f232887074941bb131660c8560d5eb8117
Sindhu Elango <sindhuocs22@gmail.com>
2020-03-09 16:51:11 +0530

    Test cases added

--- ios AssignmentTests/ios_AssignmentTests.swift
+++ ios AssignmentTests/ios_AssignmentTests.swift
@@ -5,30 +5,47 @@
 //  Created by INFOSYS on 06/03/20.
 //  Copyright © 2020 INFOSYS. All rights reserved.
 //
-
 import XCTest
 @testable import ios_Assignment
 
 class IosAssignmentTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  func testInitMyTable() {
+    let vc = CountryList()
+    XCTAssertNotNil(vc.tableCountry)
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  }
+  func testLoadTableViewWithDataSource() {
+    let vc = CountryList()
+    XCTAssertTrue(vc.tableCountry.dataSource?.isEqual(vc) ?? true)
+  }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+  func testLoadTableViewWithDelegate() {
+    let vc = CountryList()
+    XCTAssertTrue(vc.tableCountry.dataSource?.isEqual(vc) ?? true)
+  }
+  // MARK: - Checks model
+  //Below methods checks json response matches with dummy data
+  func testDecodableDatawithDummyValues() {
+    guard let url = URL(string: Constants.Baseurl) else { return }
+    let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+      guard let dataResponse = data,
+        error == nil else {
+          print(error?.localizedDescription ?? "Response Error")
+          return }
+      do {
+        //here dataResponse received from a network request
+        if let value = String(data: dataResponse, encoding: String.Encoding.ascii) {
+          if let jsonData = value.data(using: String.Encoding.utf8) {
+            let decodedData = try JSONDecoder().decode(CountryModel.self, from: jsonData)
+            XCTAssertEqual(decodedData.title, "About Canada")
+            XCTAssertLessThan(decodedData.rows?.count ?? 0, 0)
+          }
         }
+      } catch let parsingError {
+        print("Error", parsingError)
+      }
     }
-
+    task.resume()
+  }
 }
